8630a20f7b4c1066595c25077d43e7c6f0b94dbf
Lishan <149688695+Lishan6@users.noreply.github.com>
2024-07-11 19:37:03 +0200

    Update recipe.js

--- routes/recipe.js
+++ routes/recipe.js
@@ -5,5 +5,24 @@ import recipeControllers from '../controllers/recipe.js';
 const router = express.Router();
 
 // routes
+router.post('/register',(req,res)=>{
+    const body=req.body
+    const userName=body.userName
+    const password=body.password
+    console.log(userName,password);
+    res.json({userName,password}) 
+})
+
+router.post('/login',(req,res)=>{
+  
+})
+router.post('/logout',(req,res)=>{
+  
+})
+
+router.post('/recipes', isAuthenticated,(req,res)=>{
+  
+})
+
 
 export default router;
